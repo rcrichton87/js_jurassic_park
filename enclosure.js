ad7118e3d38c8a122f10be2cbb7c882f64cdc409
Ross Crichton <rcrichton87@googlemail.com>
2017-04-04 16:18:23 +0100

    enclosures start with no dinosaurs

---
+++ enclosure.js
@@ -0,0 +1,5 @@
+var Enclosure = function(){
+  this.dinosaurs = [];
+}
+
+module.exports = Enclosure;
---
+++ specs/enclosure_specs.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var Enclosure = require('../enclosure.js');
+var Dinosaur = require('../dinosaur.js');
+
+describe('Enclosure', function(){
+
+  var enclosure;
+  var trex1;
+  var trex2;
+  var velociraptor;
+  var stegosaurus;
+
+  beforeEach(function(){
+    enclosure = new Enclosure();
+    trex1 = new Dinosaur("T-Rex", 1);
+    trex2 = new Dinosaur("T-Rex", 1);
+    velociraptor = new Dinosaur("Velociraptor", 5);
+    stegosaurus = new Dinosaur("Stegosaurus", 3);
+  })
+
+  it('starts empty', function(){
+    assert.strictEqual(0, enclosure.dinosaurs.length);
+  })
+
+})
